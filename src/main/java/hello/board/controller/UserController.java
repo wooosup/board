30d2039cad5699464d8d21580aa51922814acd93
useop <useop0821@gmail.com>
2024-11-01 23:01:20 +0900

    로직 수정

--- src/main/java/hello/board/controller/UserController.java
+++ src/main/java/hello/board/controller/UserController.java
@@ -1,15 +1,16 @@
 package hello.board.controller;
 
-import hello.board.domain.User;
-import hello.board.domain.UserForm;
+import hello.board.domain.user.UserForm;
 import hello.board.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
 
 @Controller
 @RequiredArgsConstructor
@@ -17,18 +18,25 @@ public class UserController {
 
     private final UserService userService;
 
-    @GetMapping("/users/new")
-    public String createForm(Model model) {
-        model.addAttribute("userForm", new UserForm());
-        return "users/createUserForm";
+    @GetMapping("/login")
+    public String login(){
+        return "users/loginForm";
     }
 
-    @PostMapping("/users/new")
-    public String create(@Validated UserForm form, BindingResult result) {
-        if (result.hasErrors()) {
-            return "users/createUserForm";
-        }
-        userService.createUser(form.getUsername(), form.getPassword());
-        return "redirect:/";
+    @GetMapping("/signup")
+    public String signupForm() {
+        return "users/signup";
+    }
+
+    @PostMapping("/signup")
+    public String signup(UserForm form) {
+        userService.saveUser(form);
+        return "redirect:/login";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        new SecurityContextLogoutHandler().logout(request, response, SecurityContextHolder.getContext().getAuthentication());
+        return "redirect:/login";
     }
 }
