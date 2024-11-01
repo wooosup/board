195762a559d93540d56e56682797732eaffd818a
useop <useop0821@gmail.com>
2024-11-01 22:57:42 +0900

    시간 '분 전'으로 변경

---
+++ src/main/java/hello/board/domain/TimeUtil.java
@@ -0,0 +1,18 @@
+package hello.board.domain;
+
+import org.ocpsoft.prettytime.PrettyTime;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.Locale;
+
+public class TimeUtil {
+
+    private static final PrettyTime PRETTY_TIME = new PrettyTime(Locale.KOREAN);
+
+    public static String getTime(LocalDateTime dateTime) {
+        Date date = Date.from(dateTime.atZone(ZoneId.systemDefault()).toInstant());
+        return PRETTY_TIME.format(date);
+    }
+}
