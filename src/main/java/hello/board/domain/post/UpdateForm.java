5cdecb5d059c97621acb03b88b86df28f30312cb
useop <useop0821@gmail.com>
2024-11-01 22:59:02 +0900

    업데이트폼 추가

---
+++ src/main/java/hello/board/domain/post/UpdateForm.java
@@ -0,0 +1,24 @@
+package hello.board.domain.post;
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UpdateForm {
+
+    private Long id;
+
+    @NotEmpty(message = "제목은 필수입니다.")
+    private String title;
+
+    @NotEmpty(message = "내용은 필수입니다.")
+    private String content;
+
+    public UpdateForm(PostForm form) {
+        this.id = form.getId();
+        this.title = form.getTitle();
+        this.content = form.getContent();
+    }
+}
