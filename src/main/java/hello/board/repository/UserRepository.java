56d80af49811a728a4f7213a0219d34e02cd9437
useop <useop0821@gmail.com>
2024-11-01 23:00:57 +0900

    Optional로 변경

--- src/main/java/hello/board/repository/UserRepository.java
+++ src/main/java/hello/board/repository/UserRepository.java
@@ -1,11 +1,11 @@
 package hello.board.repository;
 
-import hello.board.domain.User;
+import hello.board.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    List<User> findByUsername(String username);
+    Optional<User> findByUsername(String username);
 }
