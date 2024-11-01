a119aedc04631cf3b0df27a095e3eb40c99e6c1d
useop <useop0821@gmail.com>
2024-11-01 23:01:38 +0900

    수정, 삭제 추가

--- src/main/java/hello/board/controller/PostController.java
+++ src/main/java/hello/board/controller/PostController.java
@@ -1,67 +1,75 @@
 package hello.board.controller;
 
-import hello.board.domain.Comment;
-import hello.board.domain.Post;
-import hello.board.domain.PostForm;
-import hello.board.service.CommentService;
+import hello.board.domain.post.PostForm;
+import hello.board.domain.post.UpdateForm;
+import hello.board.domain.user.User;
 import hello.board.service.PostService;
-import hello.board.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 public class PostController {
 
     private final PostService postService;
-    private final CommentService commentService;
-    private final UserService userService;
 
-    @GetMapping("/posts/new")
-    public String createForm(Model model) {
-        model.addAttribute("postForm", new PostForm());
-        model.addAttribute("users", userService.findAll());
-        return "posts/createPostForm";
-    }
-
-
-    @PostMapping("/posts/new")
-    public String createPost(@Validated @ModelAttribute PostForm form, BindingResult result, Model model, RedirectAttributes redirectAttributes) {
-
-        if (result.hasErrors()) {
-            model.addAttribute("users", userService.findAll());
-            return "posts/createPostForm";
-        }
-
-        postService.createPost(form.getTitle(), form.getContent(), form.getUserId());
-        redirectAttributes.addFlashAttribute("message", "게시글이 생성되었습니다.");
-        return "redirect:/posts";
-    }
-
-    @GetMapping("/posts")
-    public String listPosts(Model model) {
-        List<Post> posts = postService.findAllPosts();
-        model.addAttribute("posts", posts);
+    @GetMapping("/")
+    public String postList(Model model) {
+        model.addAttribute("posts", postService.findAll());
         return "posts/postList";
     }
 
-    @GetMapping("/posts/{postId}")
+    @GetMapping("/post/create")
+    public String createPostForm(Model model) {
+        model.addAttribute("form", new PostForm());
+        return "posts/createPostForm";
+    }
+
+    @PostMapping("/post/create")
+    public String createPost(@Validated @ModelAttribute("form") PostForm form,
+                             BindingResult result, Authentication auth,
+                             RedirectAttributes redirect) {
+        if (result.hasErrors()) {
+            return "posts/createPostForm";
+        }
+        Long postId = postService.savePost(form, auth.getName());
+        redirect.addFlashAttribute("successMessage", "게시글이 작성되었습니다.");
+        return "redirect:/post/" + postId;
+    }
+
+    @GetMapping("/post/{postId}")
     public String viewPost(@PathVariable Long postId, Model model) {
-        Post post = postService.findPostById(postId);
-        List<Comment> comments = commentService.findCommentsByPostId(postId);
-        model.addAttribute("post", post);
-        model.addAttribute("comments", comments);
+        model.addAttribute("post", postService.findByPostId(postId));
         return "posts/viewPost";
+    }
+
+    @GetMapping("/post/edit/{postId}")
+    public String updatePostForm(@PathVariable Long postId, Model model) {
+
+        PostForm form = postService.findPostForm(postId);
+        model.addAttribute("form", form);
+        model.addAttribute("postId", postId);
+        return "posts/updatePostForm";
+    }
+
+    @PostMapping("/post/edit/{postId}")
+    public String updatePost(@PathVariable Long postId, @ModelAttribute("form") PostForm form, Authentication auth) {
+        form.setId(postId);
+        postService.updatePost(new UpdateForm(form), auth.getName());
+        return "redirect:/post/" + postId;
+    }
+
+    @PostMapping("/post/delete/{postId}")
+    public String deletePost(@PathVariable Long postId, Authentication auth) {
+        postService.deletePost(postId, auth.getName());
+        return "redirect:/";
     }
 }
