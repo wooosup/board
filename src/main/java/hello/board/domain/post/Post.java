5d6ee4016dac92097d48c24c53184ecadb68d847
useop <useop0821@gmail.com>
2024-11-01 22:59:18 +0900

    생성자에서 빌더로 변경

---
+++ src/main/java/hello/board/domain/post/Post.java
@@ -0,0 +1,63 @@
+package hello.board.domain.post;
+
+import hello.board.domain.TimeUtil;
+import hello.board.domain.comment.Comment;
+import hello.board.domain.user.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static jakarta.persistence.CascadeType.ALL;
+import static jakarta.persistence.FetchType.LAZY;
+
+@Entity
+@Getter
+@Table(name = "posts")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "post_id")
+    private Long id;
+
+    private String title;
+    private String content;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @OneToMany(mappedBy = "post", cascade = ALL, orphanRemoval = true)
+    private List<Comment> comments = new ArrayList<>();
+
+    @Column(name = "post_date")
+    private LocalDateTime postDate;
+
+    @Builder
+    public Post(String title, String content, User user) {
+        this.title = title;
+        this.content = content;
+        this.setUser(user);
+        this.postDate = LocalDateTime.now();
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public void updatePost(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public String getFormattedPostDate() {
+        return TimeUtil.getTime(this.postDate);
+    }
+}
