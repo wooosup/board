eabfe97c4fb100d4415abfc255e11f2647111c9c
useop <useop0821@gmail.com>
2024-11-05 18:38:29 +0900

    .

--- src/main/java/hello/board/repository/PostRepository.java
+++ src/main/java/hello/board/repository/PostRepository.java
@@ -2,8 +2,13 @@ package hello.board.repository;
 
 
 import hello.board.domain.post.Post;
+import hello.board.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    List<Post> findByUser(User user);
 }
