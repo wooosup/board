05a50990cfeed74fbe9ed729f4efd3f6acb8c3da
useop <useop0821@gmail.com>
2024-11-09 01:52:55 +0900

    닉네임 반환 추가

--- src/main/java/hello/board/service/EntityFinder.java
+++ src/main/java/hello/board/service/EntityFinder.java
@@ -1,9 +1,11 @@
 package hello.board.service;
 
 import hello.board.domain.comment.Comment;
+import hello.board.domain.message.Message;
 import hello.board.domain.post.Post;
 import hello.board.domain.user.User;
 import hello.board.repository.CommentRepository;
+import hello.board.repository.MessageRepository;
 import hello.board.repository.PostRepository;
 import hello.board.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +19,7 @@ public class EntityFinder {
     private final UserRepository userRepository;
     private final PostRepository postRepository;
     private final CommentRepository commentRepository;
+    private final MessageRepository messageRepository;
 
     public Post getPost(Long postId) {
         return postRepository.findById(postId)
@@ -31,5 +34,17 @@ public class EntityFinder {
     public User getLoginUser(String loginId) {
         return userRepository.findByUsername(loginId)
                 .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
+    }
+
+    public Message getMessage(Long messageId) {
+        return messageRepository.findById(messageId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 쪽지를 찾을 수 없습니다."));
+    }
+
+    //  id를 닉네임으로 반환
+    public String getUserNickname(String username) {
+        return userRepository.findByUsername(username)
+                .map(User::getNickname)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자를 찾을 수 없습니다."));
     }
 }
