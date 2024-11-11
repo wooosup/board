1196586be0b5655146235634648cb46711ec6f53
seop <useop0821@gmail.com>
2024-11-11 18:24:16 +0900

    S3 추가

---
+++ src/main/java/hello/board/service/S3Service.java
@@ -0,0 +1,70 @@
+package hello.board.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+@Profile("prod")
+public class S3Service implements FileStore{
+
+    @Value("${cloud.aws.s3.bucketName}")
+    private String bucketName;
+
+    private final S3Client s3Client;
+
+    public String uploadFile(MultipartFile file) {
+        String fileName = createFileName(file.getOriginalFilename());
+        try (InputStream inputStream = file.getInputStream()) {
+            s3Client.putObject(PutObjectRequest.builder()
+                            .bucket(bucketName)
+                            .key(fileName)
+                            .acl("public-read")
+                            .build(),
+                    RequestBody.fromInputStream(inputStream, file.getSize()));
+        } catch (IOException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "파일 업로드에 실패했습니다.", e);
+        }
+        return fileName;
+    }
+
+
+    public void deleteFile(String fileName) {
+        s3Client.deleteObject(DeleteObjectRequest.builder()
+                .bucket(bucketName)
+                .key(fileName)
+                .build());
+    }
+
+    @Override
+    public String generateFileUrl(String fileName) {
+        URL url = s3Client.utilities().getUrl(b -> b.bucket(bucketName).key(fileName));
+        return url.toString();
+    }
+
+    private String createFileName(String fileName) {
+        return UUID.randomUUID().toString().concat(getFileExtension(fileName));
+    }
+
+    private String getFileExtension(String fileName) {
+        try {
+            return fileName.substring(fileName.lastIndexOf("."));
+        } catch (StringIndexOutOfBoundsException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "잘못된 형식의 파일(" + fileName + ") 입니다.");
+        }
+    }
+}
