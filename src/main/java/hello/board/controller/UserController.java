8762b1faecb2973c6eeaf798acd8f192017e4354
seop <useop0821@gmail.com>
2024-11-12 02:45:22 +0900

    쪽지 추가

--- src/main/java/hello/board/controller/UserController.java
+++ src/main/java/hello/board/controller/UserController.java
@@ -1,6 +1,9 @@
 package hello.board.controller;
 
+import hello.board.domain.message.MessageDto;
 import hello.board.domain.user.UserForm;
+import hello.board.domain.user.UserPostsAndCommentsDto;
+import hello.board.service.MessageService;
 import hello.board.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -8,8 +11,15 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.security.Principal;
+import java.util.List;
 
 
 @Controller
@@ -17,9 +27,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class UserController {
 
     private final UserService userService;
+    private final MessageService messageService;
 
     @GetMapping("/login")
-    public String login(){
+    public String login() {
         return "users/loginForm";
     }
 
@@ -29,14 +40,36 @@ public class UserController {
     }
 
     @PostMapping("/signup")
-    public String signup(UserForm form) {
-        userService.saveUser(form);
+    public String signup(@Validated @ModelAttribute("form") UserForm form, Model model) {
+        try {
+            userService.saveUser(form);
+        } catch (IllegalArgumentException e) {
+            model.addAttribute("errorMessage", e.getMessage());
+            return "users/signup";
+        }
         return "redirect:/login";
     }
-
     @GetMapping("/logout")
     public String logout(HttpServletRequest request, HttpServletResponse response) {
         new SecurityContextLogoutHandler().logout(request, response, SecurityContextHolder.getContext().getAuthentication());
         return "redirect:/login";
+    }
+
+    @GetMapping("/mypage")
+    public String myPage(Model model, Principal pri) {
+        String username = pri.getName();
+        UserPostsAndCommentsDto userContent = userService.getUserPostsAndComments(username);
+        model.addAttribute("posts", userContent.getPosts());
+        model.addAttribute("comments", userContent.getComments());
+
+        // 받은 쪽지 조회
+        List<MessageDto> receivedMessages = messageService.getReceivedMessages(username);
+        model.addAttribute("receivedMessages", receivedMessages);
+
+        // 보낸 쪽지 조회
+        List<MessageDto> sentMessages = messageService.getSentMessages(username);
+        model.addAttribute("sentMessages", sentMessages);
+
+        return "users/mypage";
     }
 }
