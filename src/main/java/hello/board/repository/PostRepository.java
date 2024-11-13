d91e2564ab8461c8719a312bd7f32938508a954d
seop <useop0821@gmail.com>
2024-11-14 01:04:06 +0900

    수정

--- src/main/java/hello/board/repository/PostRepository.java
+++ src/main/java/hello/board/repository/PostRepository.java
@@ -10,9 +10,5 @@ import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
-    List<Post> findByUser(User user);
-
-    List<Post> findAllByOrderByPostDateDesc();
-
     List<Post> findByUserOrderByPostDateDesc(User user);
 }
