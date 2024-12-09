30b251af4362e26f31d883175ce250e1102deae3
seop <useop0821@gmail.com>
2024-12-10 06:26:34 +0900

    좋아요

---
+++ src/main/java/hello/board/repository/LikeRepository.java
@@ -0,0 +1,16 @@
+package hello.board.repository;
+
+import hello.board.domain.like.Like;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface LikeRepository extends JpaRepository<Like, Long> {
+
+    @Query("SELECT l FROM Like l WHERE l.post.id = :postId AND l.user.username = :username")
+    Optional<Like> findByPostIdAndUsername(@Param("postId") Long postId, @Param("username") String username);
+
+    Long countByPostId(Long postId);
+}
