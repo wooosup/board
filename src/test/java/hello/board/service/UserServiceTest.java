1537b090d9f831da31659a15650efb05d0e0c50c
seop <useop0821@gmail.com>
2024-12-22 06:53:05 +0900

    닉네임 중복 검사 추가

--- src/test/java/hello/board/service/UserServiceTest.java
+++ src/test/java/hello/board/service/UserServiceTest.java
@@ -2,6 +2,7 @@ package hello.board.service;
 
 import hello.board.domain.user.UserForm;
 import hello.board.service.user.UserService;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,6 +17,7 @@ class UserServiceTest {
     @Autowired
     private UserService userService;
 
+    @DisplayName("아이디 중복 검사")
     @Test
     void duplicateUsername() throws Exception {
         //given
@@ -36,5 +38,28 @@ class UserServiceTest {
         assertThatThrownBy(() -> userService.saveUser(userForm2))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("이미 존재하는 아이디입니다.");
+    }    
+    
+    @DisplayName("닉네임 중복 검사")
+    @Test
+    void duplicateNickname() throws Exception {
+        //given
+        UserForm userForm1 = new UserForm();
+        userForm1.setUsername("testUser1");
+        userForm1.setNickname("nickname");
+        userForm1.setPassword("password");
+
+        UserForm userForm2 = new UserForm();
+        userForm2.setUsername("testUser2");
+        userForm2.setNickname("nickname");
+        userForm2.setPassword("password");
+
+        //when
+        userService.saveUser(userForm1);
+
+        //then
+        assertThatThrownBy(() -> userService.saveUser(userForm2))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("이미 존재하는 닉네임입니다.");
     }
 }
