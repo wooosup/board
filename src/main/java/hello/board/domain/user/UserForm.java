d2a79c4be9b168f7702d24af720f80dc289b771a
seop <useop0821@gmail.com>
2024-12-22 06:53:49 +0900

    validation 추가

--- src/main/java/hello/board/domain/user/UserForm.java
+++ src/main/java/hello/board/domain/user/UserForm.java
@@ -2,6 +2,7 @@ package hello.board.domain.user;
 
 import hello.board.domain.Role;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,9 +14,11 @@ public class UserForm {
     private String nickname;
 
     @NotEmpty(message = "아이디는 필수입니다.")
+    @Size(min = 5, message = "아이디는 5자리 이상이어야 합니다.")
     private String username;
 
     @NotEmpty(message = "비밀번호는 필수입니다.")
+    @Size(min = 8, message = "비밀번호는 8자리 이상이어야 합니다.")
     private String password;
 
     public User toEntity(String encodedPassword) {
