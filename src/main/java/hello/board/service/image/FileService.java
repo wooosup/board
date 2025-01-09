99ab179e627852cbb66656811920345e555a8a22
seop <useop0821@gmail.com>
2025-01-10 04:43:53 +0900

    feat: InMemoryMultipartFile 분리

--- src/main/java/hello/board/service/image/FileService.java
+++ src/main/java/hello/board/service/image/FileService.java
@@ -1,10 +1,11 @@
 package hello.board.service.image;
 
+import hello.board.exception.FileStorageException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.*;
+import java.io.IOException;
 
 @Service
 @RequiredArgsConstructor
@@ -14,59 +15,22 @@ public class FileService {
 
     public String uploadFile(String originalFileName, byte[] fileData) throws IOException {
         MultipartFile multipartFile = new InMemoryMultipartFile(originalFileName, fileData);
-        return fileStore.uploadFile(multipartFile);
+        try {
+            return fileStore.uploadFile(multipartFile);
+        } catch (IOException e) {
+            throw new FileStorageException("파일 업로드에 실패했습니다.", e);
+        }
     }
 
-    public void deleteFile(String filePath) throws IOException {
-        fileStore.deleteFile(filePath);
+    public void deleteFile(String fileName) {
+        try {
+            fileStore.deleteFile(fileName);
+        } catch (IOException e) {
+            throw new FileStorageException("파일 삭제에 실패했습니다.", e);
+        }
     }
 
-    public String generateFileUrl(String filePath) {
-        return fileStore.generateFileUrl(filePath);
-    }
-
-    private record InMemoryMultipartFile(String originalFileName, byte[] fileData) implements MultipartFile {
-
-        @Override
-        public String getName() {
-            return originalFileName;
-        }
-
-        @Override
-        public String getOriginalFilename() {
-            return originalFileName;
-        }
-
-        @Override
-        public String getContentType() {
-            return "application/octet-stream";
-        }
-
-        @Override
-        public boolean isEmpty() {
-            return fileData.length == 0;
-        }
-
-        @Override
-        public long getSize() {
-            return fileData.length;
-        }
-
-        @Override
-        public byte[] getBytes() {
-            return fileData;
-        }
-
-        @Override
-        public InputStream getInputStream() {
-            return new ByteArrayInputStream(fileData);
-        }
-
-        @Override
-        public void transferTo(File dest) throws IOException {
-            try (FileOutputStream fos = new FileOutputStream(dest)) {
-                fos.write(fileData);
-            }
-        }
+    public String generateFileUrl(String fileName) {
+        return fileStore.generateFileUrl(fileName);
     }
 }
