fb8842c55dd32beaba0a5f1f92e2cf0e9d1ff86d
seop <useop0821@gmail.com>
2025-01-10 04:40:23 +0900

    feat: 이미지 파일 예외처리 생성

---
+++ src/main/java/hello/board/exception/FileStorageException.java
@@ -0,0 +1,11 @@
+package hello.board.exception;
+
+public class FileStorageException extends RuntimeException {
+    public FileStorageException(String message) {
+        super(message);
+    }
+
+    public FileStorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
