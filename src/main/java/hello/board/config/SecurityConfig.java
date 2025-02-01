672fb9d65748fbe158fef63ed84b661421d9b633
seop <useop0821@gmail.com>
2025-02-02 08:29:18 +0900

    chore: 람다 메서드참조로 변경

--- src/main/java/hello/board/config/SecurityConfig.java
+++ src/main/java/hello/board/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -23,7 +24,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http
-                .csrf((csrf) -> csrf.disable());
+                .csrf(AbstractHttpConfigurer::disable);
 
         http
                 .formLogin((login) -> login
@@ -32,7 +33,7 @@ public class SecurityConfig {
                         .failureUrl("/login?error=true")
                         .permitAll());
         http
-                .httpBasic((basic) -> basic.disable());
+                .httpBasic(AbstractHttpConfigurer::disable);
         http
                 .authorizeHttpRequests((auth) -> auth
                         .requestMatchers("/","/post/{postId}/**").permitAll()
