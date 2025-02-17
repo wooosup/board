e71f90cb622a5ac7b524117f898195143e601fce
seop <useop0821@gmail.com>
2025-02-17 23:02:31 +0900

    test: postService 테스트

---
+++ src/test/java/hello/board/service/post/PostServiceTest.java
@@ -0,0 +1,124 @@
+package hello.board.service.post;
+
+import hello.board.domain.Role;
+import hello.board.domain.entity.post.Post;
+import hello.board.domain.entity.user.User;
+import hello.board.domain.repository.PostRepository;
+import hello.board.domain.repository.UserRepository;
+import hello.board.exception.EntityNotFoundException;
+import hello.board.service.post.dto.PostDetailDto;
+import hello.board.service.post.dto.PostForm;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@SpringBootTest
+@Transactional
+class PostServiceTest {
+
+    @Autowired
+    private PostService postService;
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void savePost() throws Exception {
+        //given
+        User user = User.builder()
+                .username("wss3325")
+                .nickname("keke")
+                .password("1234")
+                .grade(Role.USER)
+                .build();
+        userRepository.save(user);
+
+        PostForm form = PostForm.builder()
+                .title("제목")
+                .content("내용")
+                .build();
+
+        //when
+        postService.savePost(form, null, user.getUsername());
+
+        //then
+        Post savedPost = postRepository.findAll().get(0);
+        assertThat(savedPost.getTitle()).isEqualTo("제목");
+        assertThat(savedPost.getContent()).isEqualTo("내용");
+    }
+
+    @Test
+    void findPost() throws Exception {
+        //given
+        User user = User.builder()
+                .username("wss3325")
+                .nickname("keke")
+                .password("1234")
+                .grade(Role.USER)
+                .build();
+        userRepository.save(user);
+        Post post = Post.builder()
+                .title("제목")
+                .content("내용")
+                .user(user)
+                .build();
+        postRepository.save(post);
+
+        //when
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        PostDetailDto response = postService.findByPostId(post.getId(), request);
+
+        //then
+        assertThat(response.getTitle()).isEqualTo("제목");
+        assertThat(response.getContent()).isEqualTo("내용");
+    }
+
+    @Test
+    void findPostException() throws Exception {
+        //given
+        Long postId = 1L;
+
+        // expect
+        assertThatThrownBy(() -> postService.findByPostId(postId, new MockHttpServletRequest()))
+                .isInstanceOf(EntityNotFoundException.class)
+                .hasMessage("게시글을 찾을 수 없습니다.");
+    }
+
+    @Test
+    void updatePost() throws Exception {
+        //given
+        User user = User.builder()
+                .username("wss3325")
+                .nickname("keke")
+                .password("1234")
+                .grade(Role.USER)
+                .build();
+        userRepository.save(user);
+        Post post = Post.builder()
+                .title("제목")
+                .content("내용")
+                .user(user)
+                .build();
+        postRepository.save(post);
+
+        PostForm form = PostForm.builder()
+                .title("수정된 제목")
+                .content("수정된 내용")
+                .build();
+
+        //when
+        postService.updatePost(post.getId(), form, null, user.getUsername(), null);
+
+        //then
+        assertThat(post.getTitle()).isEqualTo("수정된 제목");
+        assertThat(post.getContent()).isEqualTo("수정된 내용");
+    }
+}
