7d27ea81e12a659e5e22fa72437989201e19dfbb
seop <useop0821@gmail.com>
2025-02-19 18:30:14 +0900

    refactor: 메서드 추출

--- src/main/java/hello/board/service/comment/CommentService.java
+++ src/main/java/hello/board/service/comment/CommentService.java
@@ -1,12 +1,12 @@
 package hello.board.service.comment;
 
 import hello.board.domain.entity.comment.Comment;
-import hello.board.service.comment.dto.CommentDto;
-import hello.board.service.comment.dto.CommentForm;
 import hello.board.domain.entity.post.Post;
 import hello.board.domain.entity.user.User;
 import hello.board.domain.repository.CommentRepository;
 import hello.board.service.EntityFinder;
+import hello.board.service.comment.dto.CommentDto;
+import hello.board.service.comment.dto.CommentForm;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -61,52 +61,49 @@ public class CommentService {
 
         checkAuthorization(findComment, user);
 
-        if (findComment.getParent() == null) {
-            // 최상위 댓글인 경우 -> 소프트 삭제
+        if (hasChildren(findComment)) {
             findComment.markAsDeleted();
-            // 자식이 모두 삭제되면 부모도 물리 삭제
             removeParentIfPossible(findComment);
         } else {
-            // 대댓글인 경우 -> 즉시 물리 삭제
             commentRepository.delete(findComment);
-            // 부모 댓글이 "소프트 삭제 + 남은 자식 없음" 이면 부모도 삭제
             removeParentIfPossible(findComment.getParent());
         }
     }
 
-    /**
-     * 상위 부모 댓글을 재귀적으로 삭제할 수 있는지 체크
-     */
     private void removeParentIfPossible(Comment parent) {
         if (parent == null) {
             return;
         }
-        
-        // 부모가 아직 소프트 삭제되지 않았다면 물리 삭제 불가
+
         if (!parent.isDeleted()) {
             return;
         }
 
-        // 자식 댓글이 하나라도 남아 있으면 물리 삭제 불가
         if (!parent.getChildren().isEmpty()) {
             return;
         }
 
-        // 위 두 조건을 모두 만족하면 부모도 물리 삭제
         commentRepository.delete(parent);
 
-        // 재귀 검사
         removeParentIfPossible(parent.getParent());
+    }
+
+    private static boolean hasChildren(Comment comment) {
+        return !comment.getChildren().isEmpty();
     }
 
     private static List<CommentDto> getCommentDto(Post post) {
         List<Comment> comments = post.getComments().stream()
-                .filter(c -> c.getParent() == null)
+                .filter(CommentService::hasNotParent)
                 .toList();
 
         return comments.stream()
                 .map(CommentDto::of)
                 .toList();
+    }
+
+    private static boolean hasNotParent(Comment comment) {
+        return comment.getParent() == null;
     }
 
     private static void checkAuthorization(Comment findComment, User user) {
