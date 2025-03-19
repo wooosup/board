4ce985d21213e469e46fa05cfad75d2454b1e5a6
seop <useop0821@gmail.com>
2025-03-19 23:34:30 +0900

    feat: countByParentIdAndDeletedFalse 추가

--- src/main/java/hello/board/domain/repository/CommentRepository.java
+++ src/main/java/hello/board/domain/repository/CommentRepository.java
@@ -9,4 +9,6 @@ import java.util.List;
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     List<Comment> findByUserOrderByCreateDateTimeDesc(User user);
+
+    long countByParentIdAndDeletedFalse(Long parentId);
 }
