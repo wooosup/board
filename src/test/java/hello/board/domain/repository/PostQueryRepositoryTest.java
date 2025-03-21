c89450c3f2e71d03d6d9a8bcc845621d14003e69
seop <useop0821@gmail.com>
2025-03-22 00:16:58 +0900

    test: querydsl 페이징 테스트 코드 작성

---
+++ src/test/java/hello/board/domain/repository/PostQueryRepositoryTest.java
@@ -0,0 +1,161 @@
+package hello.board.domain.repository;
+
+import hello.board.domain.Role;
+import hello.board.domain.entity.post.Post;
+import hello.board.domain.entity.user.User;
+import hello.board.service.post.dto.MainPostDto;
+import hello.board.service.post.dto.PostSearch;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+class PostQueryRepositoryTest {
+
+    @Autowired
+    private PostQueryRepository postQueryRepository;
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void paging() throws Exception {
+        //given
+        User user = User.builder()
+                .username("wss3325")
+                .password("123456789")
+                .nickname("sup")
+                .grade(Role.USER)
+                .build();
+        userRepository.save(user);
+
+        for (int i = 0; i < 20; i++) {
+            Post post = Post.builder()
+                    .user(user)
+                    .title("제목" + i)
+                    .content("내용" + i)
+                    .build();
+            postRepository.save(post);
+        }
+
+        //when
+        PostSearch search = PostSearch.builder().build();
+        Pageable pageable = PageRequest.of(0, 10);
+        Page<MainPostDto> result = postQueryRepository.searchPosts(search, pageable);
+
+        //then
+        assertThat(result.getTotalElements()).isEqualTo(20);
+        assertThat(result.getTotalPages()).isEqualTo(2);
+        assertThat(result.getContent().get(0).getTitle()).isEqualTo("제목19");
+    }
+
+    @Test
+    void searchTitlePaging() throws Exception {
+        //given
+        User user = User.builder()
+                .username("wss3325")
+                .password("123456789")
+                .nickname("sup")
+                .grade(Role.USER)
+                .build();
+        userRepository.save(user);
+
+        for (int i = 0; i < 20; i++) {
+            Post post = Post.builder()
+                    .user(user)
+                    .title("제목" + i)
+                    .content("내용" + i)
+                    .build();
+            postRepository.save(post);
+        }
+
+        //when
+        PostSearch search = PostSearch.builder()
+                .searchField("title")
+                .keyword("5")
+                .build();
+        Pageable pageable = PageRequest.of(0, 10);
+        Page<MainPostDto> result = postQueryRepository.searchPosts(search, pageable);
+
+        //then
+        assertThat(result.getTotalElements()).isEqualTo(2);
+        assertThat(result.getTotalPages()).isEqualTo(1);
+        assertThat(result.getContent().get(0).getTitle()).isEqualTo("제목15");
+    }
+
+    @Test
+    void searchContentPaging() throws Exception {
+        //given
+        User user = User.builder()
+                .username("wss3325")
+                .password("123456789")
+                .nickname("sup")
+                .grade(Role.USER)
+                .build();
+        userRepository.save(user);
+
+        for (int i = 0; i < 20; i++) {
+            Post post = Post.builder()
+                    .user(user)
+                    .title("제목" + i)
+                    .content("내용" + i)
+                    .build();
+            postRepository.save(post);
+        }
+
+        //when
+        PostSearch search = PostSearch.builder()
+                .searchField("content")
+                .keyword("5")
+                .build();
+        Pageable pageable = PageRequest.of(0, 10);
+        Page<MainPostDto> result = postQueryRepository.searchPosts(search, pageable);
+
+        //then
+        assertThat(result.getTotalElements()).isEqualTo(2);
+        assertThat(result.getTotalPages()).isEqualTo(1);
+        assertThat(result.getContent().get(0).getContent()).isEqualTo("내용15");
+    }
+
+    @Test
+    void searchNickNamePaging() throws Exception {
+        //given
+        User user = User.builder()
+                .username("wss3325")
+                .password("123456789")
+                .nickname("sup")
+                .grade(Role.USER)
+                .build();
+        userRepository.save(user);
+        Post post = Post.builder()
+                .user(user)
+                .title("제목")
+                .content("내용")
+                .build();
+        postRepository.save(post);
+
+        //when
+        PostSearch search = PostSearch.builder()
+                .searchField("nickname")
+                .keyword("sup")
+                .build();
+        Pageable pageable = PageRequest.of(0, 10);
+        Page<MainPostDto> result = postQueryRepository.searchPosts(search, pageable);
+
+        //then
+        assertThat(result.getTotalElements()).isEqualTo(1);
+        assertThat(result.getTotalPages()).isEqualTo(1);
+        assertThat(result.getContent().get(0).getTitle()).isEqualTo("제목");
+        assertThat(result.getContent().get(0).getContent()).isEqualTo("내용");
+    }
+}
