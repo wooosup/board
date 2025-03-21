32cd1c4a0cd333a1963372551da06bd8f6c3f796
seop <useop0821@gmail.com>
2025-03-22 00:35:23 +0900

    refactor: builder로 변경

--- src/main/java/hello/board/service/user/dto/UserForm.java
+++ src/main/java/hello/board/service/user/dto/UserForm.java
@@ -4,28 +4,34 @@ import hello.board.domain.Role;
 import hello.board.domain.entity.user.User;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
+import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
 public class UserForm {
 
     @NotEmpty(message = "닉네임은 필수입니다.")
-    private String nickname;
+    private final String nickname;
 
     @NotEmpty(message = "아이디는 필수입니다.")
     @Size(min = 5, message = "아이디는 5자리 이상이어야 합니다.")
-    private String username;
+    private final String username;
 
     @NotEmpty(message = "비밀번호는 필수입니다.")
     @Size(min = 8, message = "비밀번호는 8자리 이상이어야 합니다.")
-    private String password;
+    private final String password;
+
+    @Builder
+    private UserForm(String nickname, String username, String password) {
+        this.nickname = nickname;
+        this.username = username;
+        this.password = password;
+    }
 
     public User toEntity(String encodedPassword) {
         return User.builder()
-                .username(this.username)
-                .nickname(this.nickname)
+                .username(username)
+                .nickname(nickname)
                 .password(encodedPassword)
                 .grade(Role.USER)
                 .build();
