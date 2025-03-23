4c43797d41eb4b8cd5ca5f518a11529171edbfec
seop <useop0821@gmail.com>
2025-03-24 02:04:15 +0900

    test: PostControllerTest 리팩터링

--- src/test/java/hello/board/controller/PostControllerTest.java
+++ src/test/java/hello/board/controller/PostControllerTest.java
@@ -1,9 +1,15 @@
 package hello.board.controller;
 
+import hello.board.domain.Role;
+import hello.board.domain.entity.post.Post;
+import hello.board.domain.entity.user.User;
+import hello.board.domain.repository.PostRepository;
+import hello.board.domain.repository.UserRepository;
 import hello.board.service.post.PostService;
 import hello.board.service.post.dto.MainPostDto;
 import hello.board.service.post.dto.PostSearch;
 import hello.board.service.post.dto.UpdatePostForm;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,28 +18,51 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.http.MediaType.APPLICATION_FORM_URLENCODED;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@AutoConfigureMockMvc
 @SpringBootTest
 @Transactional
-@AutoConfigureMockMvc
 class PostControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
     @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @Autowired
     private PostService postService;
+
+    @BeforeEach
+    void setUp() {
+        User user = User.builder()
+                .username("wss3325")
+                .nickname("keke")
+                .password("1234")
+                .grade(Role.USER)
+                .build();
+        userRepository.save(user);
+        Post post = Post.builder()
+                .user(user)
+                .title("제목")
+                .content("내용")
+                .build();
+        postRepository.save(post);
+    }
 
     @DisplayName("게시글의 목록 페이지를 보여준다.")
     @Test
@@ -82,13 +111,13 @@ class PostControllerTest {
 
     @DisplayName("게시글을 작성한다.")
     @Test
-    @WithMockUser(username = "12345")
+    @WithMockUser(username = "wss3325")
     void write() throws Exception {
         //given
         // when & then
         mockMvc.perform(post("/post/create")
                         .with(csrf())
-                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                        .contentType(APPLICATION_FORM_URLENCODED)
                         .param("title", "제목")
                         .param("content", "내용")
                 )
@@ -98,13 +127,13 @@ class PostControllerTest {
 
     @DisplayName("제목없이 게시글을 작성하면 오류가 발생한다.")
     @Test
-    @WithMockUser(username = "12345")
+    @WithMockUser(username = "wss3325")
     void writePostEmptyTitle() throws Exception {
         //given
         // when & then
         mockMvc.perform(post("/post/create")
                         .with(csrf())
-                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                        .contentType(APPLICATION_FORM_URLENCODED)
                         .param("title", "")
                         .param("content", "내용")
                 )
@@ -116,10 +145,10 @@ class PostControllerTest {
 
     @DisplayName("존재하지 않는 게시글은 조회할 수 없다.")
     @Test
-    @WithMockUser(username = "12345")
+    @WithMockUser(username = "wss3325")
     void viewPostNotExistence() throws Exception {
         //given
-        // when & then
+        // expect
         mockMvc.perform(get("/post/9999")
                         .with(csrf())
                 )
@@ -129,52 +158,64 @@ class PostControllerTest {
 
     @DisplayName("게시글을 조회한다.")
     @Test
-    @WithMockUser(username = "12345")
+    @WithMockUser(username = "wss3325")
     void viewPost() throws Exception {
         //given
+        Post savedPost = postRepository.findAll().get(0);
+
         // when & then
-        mockMvc.perform(get("/post/1")
+        mockMvc.perform(get("/post/" + savedPost.getId())
                         .with(csrf())
-                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                        .param("title", "제목")
-                        .param("content", "내용")
                 )
                 .andExpect(status().isOk())
+                .andExpect(model().attributeExists("post"))
                 .andExpect(view().name("posts/viewPost"))
                 .andDo(print());
     }
 
     @DisplayName("게시글을 수정한다.")
     @Test
-    @WithMockUser(username = "12345")
+    @WithMockUser(username = "wss3325")
     void updatePost() throws Exception {
         //given
+        Post savedPost = postRepository.findAll().get(0);
         UpdatePostForm updatePostForm = UpdatePostForm.builder()
                 .title("라멘")
                 .content("츠케멘")
                 .build();
 
-        //when & then
-        mockMvc.perform(post("/post/edit/1")
+        //when
+        mockMvc.perform(post("/post/edit/" + savedPost.getId())
                         .with(csrf())
-                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                        .contentType(APPLICATION_FORM_URLENCODED)
                         .param("title", updatePostForm.getTitle())
                         .param("content", updatePostForm.getContent())
                 )
                 .andExpect(status().is3xxRedirection())
                 .andDo(print());
+
+        //then
+        assertThat(savedPost.getTitle()).isEqualTo("라멘");
+        assertThat(savedPost.getContent()).isEqualTo("츠케멘");
     }
 
     @DisplayName("게시글 수정 권한이 없는 경우 오류가 발생한다.")
     @Test
     @WithMockUser(username = "1232144")
     void updatePostNotAuthority() throws Exception {
+        //given
+        Post savedPost = postRepository.findAll().get(0);
+        UpdatePostForm updatePostForm = UpdatePostForm.builder()
+                .title("라멘")
+                .content("츠케멘")
+                .build();
+
         // when & then
-        mockMvc.perform(post("/post/edit/1")
+        mockMvc.perform(post("/post/edit/" + savedPost.getId())
                         .with(csrf())
-                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                        .param("title", "수정")
-                        .param("content", "수정")
+                        .contentType(APPLICATION_FORM_URLENCODED)
+                        .param("title", updatePostForm.getTitle())
+                        .param("content", updatePostForm.getContent())
                 )
                 .andExpect(status().isInternalServerError())
                 .andDo(print());
@@ -182,13 +223,15 @@ class PostControllerTest {
 
     @DisplayName("게시글 수정 페이지를 보여준다.")
     @Test
-    @WithMockUser(username = "12345")
+    @WithMockUser(username = "wss3325")
     void updatePostForm() throws Exception {
         //given
+        Post savedPost = postRepository.findAll().get(0);
+
         //when & then
-        mockMvc.perform(get("/post/edit/1")
+        mockMvc.perform(get("/post/edit/" + savedPost.getId())
                         .with(csrf())
-                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                        .contentType(APPLICATION_FORM_URLENCODED)
                 )
                 .andExpect(status().isOk())
                 .andExpect(view().name("posts/updatePostForm"))
@@ -197,20 +240,25 @@ class PostControllerTest {
 
     @DisplayName("게시글을 삭제한다.")
     @Test
-    @WithMockUser(username = "12345")
+    @WithMockUser(username = "wss3325")
     void delete() throws Exception {
         //given
-        //when & then
-        mockMvc.perform(post("/post/delete/1")
+        Post savedPost = postRepository.findAll().get(0);
+
+        //when
+        mockMvc.perform(post("/post/delete/" + savedPost.getId())
                         .with(csrf())
                 )
                 .andExpect(status().is3xxRedirection())
                 .andDo(print());
+
+        //then
+        assertThat(postRepository.findAll()).isEmpty();
     }
 
     @DisplayName("게시글을 작성하면 DB에 저장된다.")
     @Test
-    @WithMockUser(username = "12345")
+    @WithMockUser(username = "wss3325")
     void writePost() throws Exception {
         //given
         String title = "제목";
@@ -219,7 +267,7 @@ class PostControllerTest {
         //when
         mockMvc.perform(post("/post/create")
                         .with(csrf())
-                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                        .contentType(APPLICATION_FORM_URLENCODED)
                         .param("title", title)
                         .param("content", content)
                 )
@@ -240,7 +288,7 @@ class PostControllerTest {
 
     @DisplayName("게시글에 이미지를 업로드 할 수 있다.")
     @Test
-    @WithMockUser(username = "12345")
+    @WithMockUser(username = "wss3325")
     void imageUpload() throws Exception {
         // given
         MockMultipartFile image = new MockMultipartFile(
