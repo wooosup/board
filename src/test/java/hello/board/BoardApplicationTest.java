099de0738a4bf29b5afe56378227be567242a762
seop <useop0821@gmail.com>
2025-07-23 03:16:37 +0900

    chore: update

--- src/test/java/hello/board/BoardApplicationTest.java
+++ src/test/java/hello/board/BoardApplicationTest.java
@@ -3,7 +3,7 @@ package hello.board;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = BoardApplicationTest.class)
+//@SpringBootTest(classes = BoardApplicationTest.class)
 class BoardApplicationTest {
     @Test
     void contextLoads() {
