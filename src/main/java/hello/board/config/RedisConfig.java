4b8dd399453a120948e1cbffc27b2277643e0d8e
seop <useop0821@gmail.com>
2025-07-22 23:32:25 +0900

    chore: update

--- src/main/java/hello/board/config/RedisConfig.java
+++ src/main/java/hello/board/config/RedisConfig.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
@@ -11,6 +12,7 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 @RequiredArgsConstructor
+@Profile("prod")
 public class RedisConfig {
 
 
