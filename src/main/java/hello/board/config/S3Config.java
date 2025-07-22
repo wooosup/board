662ead1acf09887e119801a17dae2bd9fbaa3347
seop <useop0821@gmail.com>
2025-07-22 23:28:30 +0900

    chore: update

--- src/main/java/hello/board/config/S3Config.java
+++ src/main/java/hello/board/config/S3Config.java
@@ -3,12 +3,14 @@ package hello.board.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
+@Profile("prod")
 public class S3Config {
 
     @Value("${cloud.aws.credentials.accessKey}")
