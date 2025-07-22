c2bbb6019492256295e1315eaeec010fe7d6c2f1
seop <useop0821@gmail.com>
2025-07-23 00:18:52 +0900

    chore: update

--- src/test/resources/schema.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS likes (
-                                     id BIGINT AUTO_INCREMENT PRIMARY KEY,
-                                     createDateTime TIMESTAMP,
-                                     modifiedDateTime TIMESTAMP,
-                                     post_post_id BIGINT,
-                                     user_user_id BIGINT,
-                                     FOREIGN KEY (post_post_id) REFERENCES posts(post_id),
-    FOREIGN KEY (user_user_id) REFERENCES users(user_id)
-    );
