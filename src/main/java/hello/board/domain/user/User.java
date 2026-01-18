685a42308dcad3c5915ad45db3957250f7b165f8
seop <useop0821@gmail.com>
2026-01-19 03:13:28 +0900

    feat: user 엔티티에 댓글 및 좋아요 관계 추가 및 생성 로직 추가

--- src/main/java/hello/board/domain/user/User.java
+++ src/main/java/hello/board/domain/user/User.java
@@ -1,8 +1,23 @@
 package hello.board.domain.user;
 
 import hello.board.domain.Role;
+import hello.board.domain.comment.Comment;
+import hello.board.domain.like.Like;
 import hello.board.domain.post.Post;
-import jakarta.persistence.*;
+import hello.board.infrastructure.web.user.request.UserForm;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,10 +25,7 @@ import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Entity
 @Getter
@@ -26,7 +38,6 @@ public class User implements UserDetails {
     @Column(name = "user_id")
     private Long id;
 
-
     @Column(unique = true, nullable = false)
     private String username;
     private String nickname;
@@ -35,8 +46,14 @@ public class User implements UserDetails {
     @Enumerated(EnumType.STRING)
     private Role grade;
 
-    @OneToMany(mappedBy = "user")
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Post> posts = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Comment> comments = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Like> likes = new ArrayList<>();
 
     @Builder
     private User(String username, String nickname, String password, Role grade) {
@@ -46,6 +63,14 @@ public class User implements UserDetails {
         this.grade = grade;
     }
 
+    public static User create(UserForm form, PasswordEncoder passwordEncoder) {
+        return User.builder()
+                .username(form.getUsername())
+                .nickname(form.getNickname())
+                .password(passwordEncoder.encode(form.getPassword()))
+                .grade(Role.USER)
+                .build();
+    }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
