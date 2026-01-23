639dc46fb8b581ae4e0269a54ca01a1a132303fa
seop <useop0821@gmail.com>
2026-01-23 16:43:52 +0900

    refactor: 페이지와 게시글 목록 및 상세 정보 검색 개선

--- src/main/java/hello/board/infrastructure/web/post/PostController.java
+++ src/main/java/hello/board/infrastructure/web/post/PostController.java
@@ -1,24 +1,31 @@
 package hello.board.infrastructure.web.post;
 
+import hello.board.infrastructure.web.comment.request.CommentForm;
 import hello.board.infrastructure.web.post.request.PostForm;
 import hello.board.infrastructure.web.post.request.UpdatePostForm;
-import hello.board.infrastructure.web.post.response.*;
-import hello.board.infrastructure.web.comment.request.CommentForm;
+import hello.board.infrastructure.web.post.response.MainPostDto;
+import hello.board.infrastructure.web.post.response.Pagination;
+import hello.board.infrastructure.web.post.response.PostResponse;
+import hello.board.infrastructure.web.post.response.PostSearch;
+import hello.board.infrastructure.web.post.response.PostWithCommentsDto;
 import hello.board.service.post.PostService;
 import jakarta.servlet.http.HttpServletRequest;
+import java.security.Principal;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.security.Principal;
-import java.util.List;
 
 
 @Controller
@@ -28,30 +35,16 @@ public class PostController {
     private final PostService postService;
 
     @GetMapping("/")
-    public String postList(@RequestParam(value = "keyword", required = false) String keyword,
-                           @RequestParam(value = "searchField", required = false) String searchField,
-                           @RequestParam(defaultValue = "0") int page, Model model) {
-        PostSearch search = PostSearch.builder()
-                .keyword(keyword)
-                .searchField(searchField)
-                .build();
-
-        Pageable pageable = PageRequest.of(page, 10);
+    public String postList(@ModelAttribute PostSearch search,
+                            @PageableDefault Pageable pageable,
+                            Model model) {
         Page<MainPostDto> posts = postService.searchPosts(search, pageable);
 
-        int pageBlockSize = 5;
-        int currentPage = posts.getNumber();
-        int totalPages = posts.getTotalPages();
-
-        int startPage = (currentPage / pageBlockSize) * pageBlockSize;
-        int endPage = Math.min(startPage + pageBlockSize - 1, totalPages - 1);
+        Pagination pagination = new Pagination(posts);
 
         model.addAttribute("posts", posts);
+        model.addAttribute("pagination", pagination);
         model.addAttribute("search", search);
-        model.addAttribute("currentPage", currentPage);
-        model.addAttribute("totalPages", totalPages);
-        model.addAttribute("startPage", startPage);
-        model.addAttribute("endPage", endPage);
 
         return "posts/postList";
     }
@@ -64,28 +57,31 @@ public class PostController {
 
     @PostMapping("/post/create")
     public String createPost(@Validated @ModelAttribute("form") PostForm form, BindingResult result,
-                             @RequestParam(value = "images", required = false) List<MultipartFile> images) {
+                             @RequestParam(value = "images", required = false) List<MultipartFile> images,
+                             Principal pri) {
         if (result.hasErrors()) {
             return "posts/createPostForm";
         }
-        PostResponse response = postService.savePost(form, images);
+        PostResponse response = postService.savePost(form, images, pri.getName());
+
         return "redirect:/post/" + response.getId();
     }
 
     @GetMapping("/post/{postId}")
     public String viewPost(@PathVariable Long postId, Model model, HttpServletRequest request) {
-        PostDetailDto postDetail = postService.findByPostId(postId, request);
-        PostWithCommentsDto postWithComments = postService.findPostWithComments(postId, request);
+        String clientIp = getIp(request);
 
-        model.addAttribute("post", postDetail);
+        PostWithCommentsDto postWithComments = postService.readPost(postId, clientIp);
+
+        model.addAttribute("post", postWithComments.getPostDetail());
         model.addAttribute("comments", postWithComments.getComments());
         model.addAttribute("commentForm", CommentForm.builder().build());
+
         return "posts/viewPost";
     }
 
     @GetMapping("/post/edit/{postId}")
     public String updatePostForm(@PathVariable Long postId, Model model) {
-
         UpdatePostForm form = postService.findPostForm(postId);
         model.addAttribute("form", form);
         model.addAttribute("postId", postId);
@@ -95,8 +91,7 @@ public class PostController {
     @PostMapping("/post/edit/{postId}")
     public String updatePost(@PathVariable Long postId, @Validated @ModelAttribute("form") UpdatePostForm form,
                              @RequestParam(value = "images", required = false) List<MultipartFile> images,
-                             @RequestParam(value = "imageIdsToDelete", required = false) List<Long> imageIdsToDelete,
-                             Principal pri) {
+                             @RequestParam(value = "imageIdsToDelete", required = false) List<Long> imageIdsToDelete) {
         postService.updatePost(postId, form, images, imageIdsToDelete);
         return "redirect:/post/" + postId;
     }
@@ -106,4 +101,11 @@ public class PostController {
         postService.deletePost(postId);
         return "redirect:/";
     }
+
+    private String getIp(HttpServletRequest request) {
+        String ip = request.getRemoteAddr();
+        String session = request.getSession().getId();
+        return ip + ":" + session;
+    }
+
 }
